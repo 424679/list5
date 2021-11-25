ff64f222f55e005f8458590ac5cdf3da0b6309d2
424679 <91246978+424679@users.noreply.github.com>
2021-11-25 15:30:55 +0800

    部分融合A、B

--- main.ts
+++ main.ts
@@ -1,49 +1,11 @@
 input.onButtonPressed(Button.A, function () {
-    if (input.buttonIsPressed(Button.B)) {
-        for (let Y = 0; Y <= 4; Y++) {
-            for (let X = 0; X <= 4; X++) {
-                if (list[Y][X] == 1) {
-                    led.plot(X, Y)
-                } else {
-                    led.unplot(X, Y)
-                }
-            }
-        }
-        list.unshift(list.pop())
-        basic.pause(100)
-    } else {
-        for (let Y = 0; Y <= 4; Y++) {
-            for (let X = 0; X <= 4; X++) {
-                if (list[Y][X] == 1) {
-                    led.plot(X, Y)
-                } else {
-                    led.unplot(X, Y)
-                }
-            }
-        }
-        for (let Y = 0; Y <= 4; Y++) {
-            item = list[Y]
-            item.unshift(item.pop())
-        }
-        basic.pause(100)
-    }
+	
 })
 input.onButtonPressed(Button.B, function () {
-    for (let Y = 0; Y <= 4; Y++) {
-        for (let X = 0; X <= 4; X++) {
-            if (list[Y][X] == 1) {
-                led.plot(X, Y)
-            } else {
-                led.unplot(X, Y)
-            }
-        }
-    }
-    list.unshift(list.pop())
-    basic.pause(100)
+	
 })
 let item: number[] = []
-let list: number[][] = []
-list = [
+let list = [
 [
 1,
 0,
@@ -89,3 +51,39 @@ for (let Y = 0; Y <= 4; Y++) {
         }
     }
 }
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        for (let index = 0; index < 25; index++) {
+            for (let Y = 0; Y <= 4; Y++) {
+                for (let X = 0; X <= 4; X++) {
+                    if (list[Y][X] == 1) {
+                        led.plot(X, Y)
+                    } else {
+                        led.unplot(X, Y)
+                    }
+                }
+            }
+            list.unshift(list.pop())
+            basic.pause(100)
+        }
+    } else if (input.buttonIsPressed(Button.A)) {
+        for (let index = 0; index < 25; index++) {
+            for (let Y = 0; Y <= 4; Y++) {
+                for (let X = 0; X <= 4; X++) {
+                    if (list[Y][X] == 1) {
+                        led.plot(X, Y)
+                    } else {
+                        led.unplot(X, Y)
+                    }
+                }
+            }
+            for (let Y = 0; Y <= 4; Y++) {
+                item = list[Y]
+                item.unshift(item.pop())
+            }
+            basic.pause(100)
+        }
+    } else {
+    	
+    }
+})
