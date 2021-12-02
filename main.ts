3a06e685ed0d289ad16054261ae39ba2a52220f1
424679 <91246978+424679@users.noreply.github.com>
2021-12-02 16:39:20 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 input.onButtonPressed(Button.A, function () {
+    enter = 1
+})
+input.onButtonPressed(Button.AB, function () {
 	
 })
 input.onButtonPressed(Button.B, function () {
-	
+    enter = 2
 })
 let item: number[] = []
+let enter = 0
 let list = [
 [
 1,
@@ -52,38 +56,32 @@ for (let Y = 0; Y <= 4; Y++) {
     }
 }
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.B)) {
-        for (let index = 0; index < 25; index++) {
-            for (let Y = 0; Y <= 4; Y++) {
-                for (let X = 0; X <= 4; X++) {
-                    if (list[Y][X] == 1) {
-                        led.plot(X, Y)
-                    } else {
-                        led.unplot(X, Y)
-                    }
+    if (enter == 1) {
+        for (let Y = 0; Y <= 4; Y++) {
+            for (let X = 0; X <= 4; X++) {
+                if (list[Y][X] == 1) {
+                    led.plot(X, Y)
+                } else {
+                    led.unplot(X, Y)
                 }
             }
-            list.unshift(list.pop())
-            basic.pause(100)
         }
-    } else if (input.buttonIsPressed(Button.A)) {
-        for (let index = 0; index < 25; index++) {
-            for (let Y = 0; Y <= 4; Y++) {
-                for (let X = 0; X <= 4; X++) {
-                    if (list[Y][X] == 1) {
-                        led.plot(X, Y)
-                    } else {
-                        led.unplot(X, Y)
-                    }
+        list.unshift(list.pop())
+        basic.pause(100)
+    } else if (enter == 2) {
+        for (let Y = 0; Y <= 4; Y++) {
+            for (let X = 0; X <= 4; X++) {
+                if (list[Y][X] == 1) {
+                    led.plot(X, Y)
+                } else {
+                    led.unplot(X, Y)
                 }
             }
-            for (let Y = 0; Y <= 4; Y++) {
-                item = list[Y]
-                item.unshift(item.pop())
-            }
-            basic.pause(100)
         }
-    } else {
-    	
+        for (let Y = 0; Y <= 4; Y++) {
+            item = list[Y]
+            item.unshift(item.pop())
+        }
+        basic.pause(100)
     }
 })
